bdb2f1d1789eba0f78e9b764f89775e4edf12102
B-TechCode <aakashchaurasiya630@gmail.com>
2026-06-23 22:03:26 +0530

    Implement bill payment saga with Kafka, orchestrator and settlement workflow

--- BillerService-develop/src/main/java/com/bill/BillPaymentServiceApplication.java
+++ BillerService-develop/src/main/java/com/bill/BillPaymentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.payments.orch;
+package com.bill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,15 +6,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"com.payments", "com.commons"})
-@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {"com.bill", "com.commons"})
+@EnableFeignClients(basePackages = "com.bill")
 @EnableKafka
 @EnableScheduling
-public class PaymentOrchestratorApplication {
+public class BillPaymentServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentOrchestratorApplication.class, args);
+		SpringApplication.run(BillPaymentServiceApplication.class, args);
 	}
 
 }
-
